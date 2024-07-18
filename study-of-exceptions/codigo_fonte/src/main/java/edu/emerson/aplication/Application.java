d4a0a68fbea423669a62b5272efb58e26ab154ca
mermaieu <menso.tr@gmail.com>
2024-07-18 20:36:41 -0300

    commit inicial

---
+++ study-of-exceptions/codigo_fonte/src/main/java/edu/emerson/aplication/Application.java
@@ -0,0 +1,22 @@
+public class Application {
+    public static void main(String[ ] args) {
+
+        int[] myNumbers = {1, 2, 3};
+
+        try {
+            System.out.println(myNumbers[10]); // error!
+            System.out.println(3/0);
+            
+            
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("Você tentou acessar uma posicão inexistente no array.");
+        
+        } catch (ArithmeticException e) {
+            System.err.println("Não existe divisão por zero");
+        }
+        System.out.println("O programa continua.");
+        
+
+      }
+    
+}
